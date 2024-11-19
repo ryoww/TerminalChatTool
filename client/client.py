d942713dd52cafb307cca9473349a6de7347c705
ryoww <ryo.shimozawa.0121@gmail.com>
2024-11-19 10:38:34 +0900

    update

--- client/client.py
+++ client/client.py
@@ -1,51 +1,57 @@
 import socketio
+import threading
 
 sio = socketio.Client()
 
+# 部屋選択中かどうかを示すイベントフラグを作成
+rooms_event = threading.Event()
 
 @sio.on('request_username')
 def request_username():
     username = input('Enter your username\n')
-    sio.emit('register_username', {'username' : username})
+    sio.emit('register_username', {'username': username})
 
 @sio.on('view_rooms')
 def view_rooms(data):
-    list_rooms = list(data['rooms'])
+    list_rooms = data['rooms']
     rooms_len = len(list_rooms)
-    list_rooms.extend(['Create and join room', 'Back'])
-    
-    for index, room in enumerate(list_rooms, 1):
-        print(f'{index} : {room}')
-    
+
+    # オプションを追加
+    list_rooms.append(['Create and join room', 0, []])
+    list_rooms.append(['Back', 0, []])
+
+    for index, room_info in enumerate(list_rooms, 1):
+        room_name = room_info[0]
+        print(f'{index}: {room_name}')
+
     while True:
         try:
-            room_num = int(input(f'Please select join room or command 1 ~ {rooms_len+2} : '))
-            
-            print('room_num : ', room_num)
-            
-            match room_num:
-                case _ if  0 < room_num <= rooms_len:
-                    sio.emit('join_room', {'room' : list_rooms[room_num][0]})
-                    
-                case _ if room_num == (rooms_len + 1):
-                    while True:
-                        new_room_name = input('Please enter new room name')
-                        
-                        if new_room_name and new_room_name not in list_rooms:
-                            sio.emit('create_room', {'room_name' : new_room_name})
-                            break
-                        
-                        else:
-                            print('Invalid room name or room already exists')
-                    break
+            room_num = int(input(f'Please select a room or command (1 ~ {rooms_len + 2}): ')) - 1
 
-                case _ if room_num == (rooms_len + 2):
-                    break
-
+            if 0 <= room_num < rooms_len:
+                # 部屋に参加
+                room_name = list_rooms[room_num][0]
+                sio.emit('join_room', {'room': room_name})
+                break
+            elif room_num == rooms_len:
+                # 新しい部屋を作成
+                while True:
+                    new_room_name = input('Please enter new room name: ')
+                    if new_room_name and new_room_name not in [room[0] for room in list_rooms]:
+                        sio.emit('create_room', {'room_name': new_room_name})
+                        break
+                    else:
+                        print('Invalid room name or room already exists.')
+                break
+            elif room_num == rooms_len + 1:
+                # 戻る
+                break
+            else:
+                print('Invalid selection.')
         except ValueError:
             print('Error: Please enter a valid integer.')
-    # print(type(list_rooms), list_rooms)
-    
+    # 部屋選択が完了したのでイベントをセット
+    rooms_event.set()
 
 @sio.on('response')
 def on_response(data):
@@ -61,17 +67,21 @@ try:
     while True:
         message = input("Enter a message (type 'exit' to quit)\n")
         if message.lower() == 'exit':
+            sio.emit('leave_room')
             sio.disconnect()
             break
         elif message == '/rooms':
+            # 部屋選択イベントをクリアして待機状態にする
+            rooms_event.clear()
             sio.emit('rooms')
+            # 部屋選択が完了するまで待機
+            rooms_event.wait()
         elif message == '/leave_room':
             sio.emit('leave_room')
         else:
-            sio.emit('message', {'text' : message})
+            sio.emit('message', {'text': message})
 
 except KeyboardInterrupt:
     print('\nClient terminated by user.\n')
-    
+    sio.emit('leave_room')
     sio.disconnect()
-    sio.wait()
--- server/server.py
+++ server/server.py
@@ -11,6 +11,13 @@ userinfos = {}
 rooms = ['Open']
 
 
+def cleanup_room(room):
+    remaining_users = [user for user in userinfos.values() if user['room'] == room]
+    if not remaining_users and room != 'Open':
+        rooms.remove(room)
+        print(f'Room "{room}" has been deleted because it is empty.')
+
+
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
@@ -21,11 +28,14 @@ def handle_connect():
 def handle_register_username(data):
     sid = request.sid
     username = data.get('username')
+    if not username:
+        emit('response', {'message': 'Username cannot be empty.'})
+        return
     join_room('Open')
-    userinfos[sid] = {'name' : username, 'room' : 'Open'}
+    userinfos[sid] = {'name': username, 'room': 'Open'}
     print(userinfos)
-    emit('response', {'message' : f'Welcom, {username}!'})
-    emit('response', {'message' : f'{username} has joined the chat.'}, room='Open', include_self=False)
+    emit('response', {'message': f'Welcome, {username}!'})
+    emit('response', {'message': f'{username} has joined the chat.'}, room='Open', include_self=False)
 
 
 @socketio.on('create_room')
@@ -38,28 +48,26 @@ def handle_create_room(data):
         leave_room(previous_room)
         join_room(room)
         userinfos[sid]['room'] = room
-        emit('response', {'message' : f'Room "{room}" created and joined.'})
-
+        emit('response', {'message': f'Room "{room}" created and joined.'})
     else:
-        emit('response', {'message' : f'Room "{room}" does not exist.'})
+        emit('response', {'message': f'Room "{room}" already exists.'})
 
 
 @socketio.on('join_room')
 def handle_join_room(data):
     sid = request.sid
     room = data.get('room')
-    
+
     if room in rooms:
         previous_room = userinfos[sid]['room']
         leave_room(previous_room)
         join_room(room)
         userinfos[sid]['room'] = room
         username = userinfos[sid]['name']
-        emit('response', {'message' : f'You have joined room "{room}"'})
-        emit('response', {'message' : f'{username} has joined the room.'}, room=room, include_self=False)
-        
+        emit('response', {'message': f'You have joined room "{room}"'})
+        emit('response', {'message': f'{username} has joined the room.'}, room=room, include_self=False)
     else:
-        emit('response', {'message' : f'Room "{room}" does not exist.'})
+        emit('response', {'message': f'Room "{room}" does not exist.'})
 
     print('join_room')
 
@@ -67,22 +75,19 @@ def handle_join_room(data):
 @socketio.on('leave_room')
 def handle_leave_room():
     sid = request.sid
-    room = userinfos[sid]['room']
-    if room != 'Open':
-        leave_room(room)
-        join_room('Open')
-        username = userinfos[sid]['name']
-        emit('response', {'message' : f'You have left room "{room}".'})
-        emit('response', {'message' : f'{username} has left the room.'}, room=room, include_self=False)
-        
-        remaining_users = [user for user in userinfos.values() if user['room'] == room]
-
-        if not remaining_users:
-            rooms.remove(room)
-            print(f'Room "{room}" has been deleted because it is empty.')
-
-    else:
-        emit('response', {'message' : 'You cannot leave the "Open" chat.'})
+    try:
+        room = userinfos[sid]['room']
+        if room != 'Open':
+            leave_room(room)
+            join_room('Open')
+            username = userinfos[sid]['name']
+            emit('response', {'message': f'You have left room "{room}".'})
+            emit('response', {'message': f'{username} has left the room.'}, room=room, include_self=False)
+            cleanup_room(room)
+        else:
+            emit('response', {'message': 'You cannot leave the "Open" chat.'})
+    except KeyError:
+        emit('response', {'message': 'User information not found.'})
 
 
 @socketio.on('rooms')
@@ -90,23 +95,25 @@ def handle_rooms():
     rooms_counts = []
 
     for room in rooms:
-        # 別にnamesは入れなくていい
         names = [user['name'] for user in userinfos.values() if user['room'] == room]
-        count = sum(1 for user in userinfos.values() if user['room'] == room)
-        room_count = [room, count, names]
-        rooms_counts.append(room_count)
+        count = len(names)
+        room_info = [room, count, names]
+        rooms_counts.append(room_info)
 
-    emit('view_rooms', {'rooms' : rooms_counts})
+    emit('view_rooms', {'rooms': rooms_counts})
 
 
 @socketio.on('message')
 def handle_message(data):
     sid = request.sid
-    username = userinfos[sid]['name']
-    text = data['text']
-    room = userinfos[sid]['room']
-    print('Received message', data)
-    emit('response', {'message' : f'{username}: {text}'},room=room, include_self=False)
+    try:
+        username = userinfos[sid]['name']
+        text = data.get('text', '')
+        room = userinfos[sid]['room']
+        print('Received message', data)
+        emit('response', {'message': f'{username}: {text}'}, room=room, include_self=False)
+    except KeyError:
+        emit('response', {'message': 'User information not found.'})
 
 
 @socketio.on('disconnect')
@@ -116,7 +123,8 @@ def handle_exit():
         username = userinfos[sid]['name']
         room = userinfos[sid]['room']
         del userinfos[sid]
-        emit('response', {'message' : f'Leave {username}'}, room=room)
+        emit('response', {'message': f'{username} has disconnected.'}, room=room)
+        cleanup_room(room)
 
 
 if __name__ == '__main__':
